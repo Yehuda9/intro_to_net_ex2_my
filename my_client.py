bf8dc669db78166a3301f99e320b28d8eb5ca705
yehuda <yehuda7701@gmail.com>
2021-11-19 20:24:25 +0200

    20241911

--- my_client.py
+++ my_client.py
@@ -12,11 +12,17 @@ def get_size_of_dir(path):
     return s, d
 
 
-def generate_message(action, path='', size_of_dirs=0, size_of_data=0, _id=''):
-    m = "action: " + action + '\n' + 'id:' + _id + '\n' + 'path:' + path + '\n' + 'size_of_dirs: ' + str(
+def generate_message(action, path='', size_of_dirs=0, size_of_data=0):
+    if action == 'upload file':
+        r_path = path.split(rel_folder_name, 1)[1].lstrip(os.path.sep)
+        path = os.path.join(os.getcwd(), r'DB', my_id, rel_folder_name, r_path)
+    elif action == 'new client':
+        path = rel_folder_name
+    m = "action: " + action + '\n' + 'id:' + my_id + '\n' + 'path:' + path + '\n' + 'size_of_dirs: ' + str(
         size_of_dirs) + '\n' + 'size_of_data: ' + str(size_of_data)
     size = str(len(m)).zfill(16)
     to_send = size + m
+    print(to_send)
     return to_send
 
 
@@ -26,17 +32,27 @@ def upload_path(path):
 
 
 def upload_file(path_to_file):
-    _message = generate_message('upload file', path_to_file, 0, getsize(path_to_file), my_id)
-    server_socket.send(_message)
+    _message = generate_message('upload file', path_to_file, 0, os.path.getsize(path_to_file))
+    print('file size: ', os.path.getsize(path_to_file))
+    server_socket.send(bytes(_message, 'utf-8'))
+    with open(path_to_file, 'rb') as f:
+        while True:
+            b = f.read(1024)
+            if not b:
+                break
+            server_socket.send(b)
 
 
 def upload_dir_to_server():
+    server_socket.send(bytes(generate_message('upload path', rel_folder_name), 'utf-8'))
     for path, dirs, files in os.walk(path_to_folder):
         for d in dirs:
             path_to_file = os.path.join(path, d)
+            print(path_to_file)
             upload_path(path_to_file)
         for file in files:
             path_to_file = os.path.join(path, file)
+            print(path_to_file)
             upload_file(path_to_file)
 
 
@@ -46,8 +62,9 @@ if __name__ == '__main__':
     path_to_folder = sys.argv[3]
     rel_folder_name = path_to_folder.split(os.sep)[-1]
     time_between_syncs = sys.argv[4]
+    my_id = '0'
     if len(sys.argv) == 6:
-        client_ID = sys.argv[5]
+        my_id = sys.argv[5]
         # create new folder and get all the files from the server.
     else:
         server_socket = socket(AF_INET, SOCK_STREAM)
@@ -56,4 +73,5 @@ if __name__ == '__main__':
         message = generate_message('new client', path_to_folder, d, s)
         server_socket.send(bytes(message, 'utf-8'))
         my_id = server_socket.recv(128).decode('utf-8')
+        print(my_id)
         upload_dir_to_server()
--- my_server.py
+++ my_server.py
@@ -9,13 +9,52 @@ def parse_message(m):
     d = {}
     l = m.split('\n')
     for e in l:
-        k, v = e.split(':')
-        d[k] = v
+        # print(e)
+        try:
+            k, v = e.split(':', 1)
+            d[k] = v
+        except:
+            pass
+    print(d)
     return d
 
 
-def get_dir_from_client():
-    pass
+def get_dir_from_client(_dict, size):
+    size_received = 0
+    while size_received < size:
+        _length = client_socket.recv(16)
+        print(_length)
+        _length = _length.decode('utf-8')
+        print(_length)
+        _length = _length.lstrip('0').rstrip('\n')
+        """numeric_filter = filter(str.isdigit, _length)
+        _length = "".join(numeric_filter)"""
+        print(_length)
+        try:
+            _length = int(_length)
+        except BaseException:
+            print('except')
+
+        _message = client_socket.recv(_length)
+        _message_dict = parse_message(_message)
+        if 'upload file' in _message_dict['action']:
+            get_file(_message_dict)
+            size_received += int(_message_dict['size_of_data'])
+        elif 'upload path' in _message_dict['action']:
+            get_path(_message_dict)
+        print("size_received: ",size_received)
+
+
+def get_path(_message_dict):
+    os.makedirs(os.path.join(os.getcwd(), 'DB', _message_dict['id'], _message_dict['path']), exist_ok=True)
+    # os.mkdirs(os.path.join(path_to_DB, _message_dict['id'], _message_dict['path']))
+
+
+def get_file(_message_dict):
+    f = open(_message_dict['path'], 'wb')
+    d = client_socket.recv(int(_message_dict['size_of_data']))
+    f.write(d)
+    f.close()
 
 
 if __name__ == '__main__':
@@ -29,12 +68,15 @@ if __name__ == '__main__':
         client_socket, client_address = server.accept()
         print('accept Connection from: ', client_address)
         length = client_socket.recv(16).decode('utf-8')
+        print(length)
         message = client_socket.recv(int(length))
         message_dict = parse_message(message)
-        if message_dict['action'] == 'new client':
+        if 'new client' in message_dict['action']:
+            # if message_dict['action'].contains('new client'):
             client_id = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(128))
             os.mkdir(os.path.join(path_to_DB, client_id))
             client_socket.send(bytes(client_id, 'utf-8'))
-            if message_dict['path']:
-                get_dir_from_client()
-        print(message_dict)
+            # get_path(message_dict)
+            if message_dict['path'] != '':
+                get_dir_from_client(message_dict, int(message_dict['size_of_data']))
+        # print(message_dict)
