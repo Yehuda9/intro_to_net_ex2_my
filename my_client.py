04a32e2102d63780065a5b78468c57d5d351a56d
yehuda <yehuda7701@gmail.com>
2021-11-20 19:01:01 +0200

    work upload delete

--- my_client.py
+++ my_client.py
@@ -1,9 +1,12 @@
-import sys, os
+import os
+import time
+import sys
 from os.path import getsize
 from socket import socket, AF_INET, SOCK_STREAM
+
 import watchdog
-from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
 
 class Watcher:
@@ -17,8 +20,9 @@ class Watcher:
         self.observer.start()
         try:
             while True:
-                time.sleep(5)
-        except:
+                time.sleep(10)
+
+        except KeyboardInterrupt:
             self.observer.stop()
             print("Error")
 
@@ -27,23 +31,35 @@ class Watcher:
 
 class Handler(FileSystemEventHandler):
     def on_created(self, event):
+        server_socket = socket(AF_INET, SOCK_STREAM)
+        server_socket.connect((server_IP, server_port))
         print("Received created event - %s." % event.src_path)
-        # upload_file(event.src_path)
+        upload_file(server_socket, event.src_path)
+        server_socket.close()
 
     def on_modified(self, event):
+        server_socket = socket(AF_INET, SOCK_STREAM)
+        server_socket.connect((server_IP, server_port))
         print("Received modified event - %s." % event.src_path)
-        # remove_file(event.src_path)
-        # upload_file(event.src_path)
+        remove_file(server_socket, event.src_path, 2)
+        upload_file(server_socket, event.src_path)
+        server_socket.close()
 
     def on_deleted(self, event):
+        server_socket = socket(AF_INET, SOCK_STREAM)
+        server_socket.connect((server_IP, server_port))
         print("Received delete event - %s." % event.src_path)
-        # remove_file(event.src_path)
+        remove_file(server_socket, event.src_path)
+        server_socket.close()
 
     def on_moved(self, event):
+        server_socket = socket(AF_INET, SOCK_STREAM)
+        server_socket.connect((server_IP, server_port))
         print("Received moved event - %s." % event.dest_path)
         print("Received moved event - %s." % event.src_path)
-        # remove_file(event.src_path)
-        # upload_file(event.dest_path)
+        remove_file(server_socket, event.src_path, 2)
+        upload_file(server_socket, event.dest_path)
+        server_socket.close()
 
 
 def get_size_of_dir(path):
@@ -58,7 +74,7 @@ def get_size_of_dir(path):
 
 
 def generate_message(action, path='', size_of_dirs=0, size_of_data=0, num_of_requests=1):
-    if action == 'upload file' or action == 'upload path':
+    if action == 'upload file' or action == 'upload path' or action == 'remove file':
         r_path = path.split(rel_folder_name, 1)[1].lstrip(os.path.sep)
         path = os.path.join(os.getcwd(), r'DB', my_id, rel_folder_name, r_path)
     elif action == 'new client':
@@ -72,38 +88,38 @@ def generate_message(action, path='', size_of_dirs=0, size_of_data=0, num_of_req
     return to_send
 
 
-def upload_path(path):
-    _message = generate_message('upload path', path)
+def remove_file(server_socket, path, num_of_requests=1):
+    _message = generate_message('remove file', path, 0, 0, num_of_requests)
     server_socket.send(_message)
 
 
-def upload_file(path_to_file):
-    _message = generate_message('upload file', path_to_file, 0, os.path.getsize(path_to_file))
+def upload_path(server_socket, path, num_of_requests=1):
+    _message = generate_message('upload path', path, 0, 0, num_of_requests)
+    server_socket.send(_message)
+
+
+def upload_file(server_socket, path_to_file, num_of_requests=1):
+    _message = generate_message('upload file', path_to_file, 0, os.path.getsize(path_to_file), num_of_requests)
     print('file size: ', os.path.getsize(path_to_file))
     server_socket.send(_message)
     with open(path_to_file, 'rb') as f:
         b = f.read()
-        print('len(b): ',len(b))
-        server_socket.send(b)
-        """return
-        while True:
-            b = f.read(1024)
-            if b == b'':
-                break
-            server_socket.send(b)"""
+        if b != b'':
+            print('len(b): ', len(b))
+            server_socket.send(b)
 
 
-def upload_dir_to_server():
+def upload_dir_to_server(server_socket):
     server_socket.send(generate_message('upload path', rel_folder_name))
     for path, dirs, files in os.walk(path_to_folder):
         for d in dirs:
             path_to_file = os.path.join(path, d)
             print(path_to_file)
-            upload_path(path_to_file)
+            upload_path(server_socket, path_to_file)
         for file in files:
             path_to_file = os.path.join(path, file)
             print(path_to_file)
-            upload_file(path_to_file)
+            upload_file(server_socket, path_to_file)
 
 
 if __name__ == '__main__':
@@ -117,14 +133,14 @@ if __name__ == '__main__':
         my_id = sys.argv[5]
         # create new folder and get all the files from the server.
     else:
-        server_socket = socket(AF_INET, SOCK_STREAM)
-        server_socket.connect((server_IP, server_port))
+        _server_socket = socket(AF_INET, SOCK_STREAM)
+        _server_socket.connect((server_IP, server_port))
         s, d, f = get_size_of_dir(path_to_folder)
         # f = num of files and subdirs + new client + rootdir
         message = generate_message('new client', path_to_folder, d, s, f + 2)
-        server_socket.send(message)
-        my_id = server_socket.recv(128).decode('utf-8')
+        _server_socket.send(message)
+        my_id = _server_socket.recv(128).decode('utf-8')
         print(my_id)
-        upload_dir_to_server()
+        upload_dir_to_server(_server_socket)
     w = Watcher()
     w.run()
--- my_server.py
+++ my_server.py
@@ -1,11 +1,13 @@
+import os
 import random
 import string
-import sys, os
+import sys
 from socket import socket, AF_INET, SOCK_STREAM
 
 
 def recv_all(n):
-    # Helper function to recv n bytes or return None if EOF is hit
+    if n == 0:
+        return b''
     data = b''
     while len(data) < n:
         packet = client_socket.recv(n - len(data))
@@ -21,7 +23,6 @@ def parse_message(m):
     d = {}
     l = m.split('\n')
     for e in l:
-        # print(e)
         try:
             k, v = e.split(':', 1)
             d[k] = v
@@ -31,55 +32,22 @@ def parse_message(m):
     return d
 
 
-def get_dir_from_client(_dict, size):
-    size_received = 0
-    while size_received < size:
-        # _length = client_socket.recv(16)
-        _length = recv_all(16)
-        print(_length)
-        _length = _length.decode('utf-8')
-        print(_length)
-        _length = _length.lstrip('0').rstrip('\n')
-        """numeric_filter = filter(str.isdigit, _length)
-        _length = "".join(numeric_filter)"""
-        print(_length)
-        try:
-            _length = int(_length)
-        except BaseException:
-            t = client_socket.recv(1024)
-            print(t)
-            print('except')
-
-        # _message = client_socket.recv(_length)
-        _message = recv_all(_length)
-        _message_dict = parse_message(_message)
-        if 'upload file' in _message_dict['action']:
-            _get_file(_message_dict)
-            size_received += int(_message_dict['size_of_data'])
-        elif 'upload path' in _message_dict['action']:
-            get_path(_message_dict)
-        print("size_received: ", size_received)
+def _remove_file(_message_dict):
+    try:
+        print(_message_dict['path'])
+        os.remove(_message_dict['path'])
+    except IsADirectoryError:
+        shutil.rmtree(_message_dict['path'])
 
 
 def _get_path(_message_dict):
     os.makedirs(os.path.join(os.getcwd(), 'DB', _message_dict['id'], _message_dict['path']), exist_ok=True)
-    # os.mkdirs(os.path.join(path_to_DB, _message_dict['id'], _message_dict['path']))
 
 
 def _get_file(_message_dict):
     f = open(_message_dict['path'], 'wb')
-    """size_of_data = int(_message_dict['size_of_data'])
-    bytes_recd = 0
-    while bytes_recd < size_of_data:
-        chunk = client_socket.recv(min(size_of_data - bytes_recd, 2048))
-        if chunk == b'':
-            break
-        f.write(chunk)
-        bytes_recd += len(chunk)
-        print('recv ', bytes_recd, ' from socket')"""
     print("size of data: ", int(_message_dict['size_of_data']))
     d = recv_all(int(_message_dict['size_of_data']))
-    # d = client_socket.recv(int(_message_dict['size_of_data']))
     if int(_message_dict['size_of_data']) != len(d):
         print('read error!!!!!!!!!!!!!!!', int(_message_dict['size_of_data']) - len(d))
     f.write(d)
@@ -97,44 +65,31 @@ if __name__ == '__main__':
     while True:
         client_socket, client_address = server.accept()
         print('accept Connection from: ', client_address)
-        # length = client_socket.recv(16).decode('utf-8')
         length = recv_all(16).decode('utf-8')
         print(length)
-        # message = client_socket.recv(int(length))
         message = recv_all(int(length))
         message_dict = parse_message(message)
         num_of_requests = int(message_dict['num_of_requests'])
         for i in range(num_of_requests):
             print(message_dict['action'])
             if 'new client' in message_dict['action']:
-                # if message_dict['action'].contains('new client'):
                 client_id = ''.join(
                     random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(128))
                 os.mkdir(os.path.join(path_to_DB, client_id))
                 client_socket.send(bytes(client_id, 'utf-8'))
-                # get_path(message_dict)
-                if message_dict['path'] != '':
-                    pass
-                    # message_dict['id'] = client_id
-                    # get_path(message_dict)
-                    # get_dir_from_client(message_dict, int(message_dict['size_of_data']))
             if 'upload file' in message_dict['action']:
                 _get_file(message_dict)
+            if 'remove file' in message_dict['action']:
+                _remove_file(message_dict)
             elif 'upload path' in message_dict['action']:
                 print('upload path!!!!!!!!!!')
                 _get_path(message_dict)
             if i == num_of_requests - 1:
                 break
-            # length = client_socket.recv(16)
             length = recv_all(16)
             print('len: ', length)
             length = length.decode('utf-8')
-            """if not length:
-                break"""
             print(length)
-            # message = client_socket.recv(int(length))
             message = recv_all(int(length))
-
             message_dict = parse_message(message)
         client_socket.close()
-        # print(message_dict)
